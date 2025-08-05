b2528788f40b5cb280eb334c21346527c1167a75
bruciexx <brucegooijer@gmail.com>
2025-08-05 20:48:30 +0200

    added 2 new projects

---
+++ hangman.py
@@ -0,0 +1,8 @@
+word = "agua"
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+letters_guessed = []
+
+def play(word):
+    print("_ " * len(word))
+
+play(word)
